bae4f49e7befdf9fa2c3255011881396072e202e
Ladislao Lainez <thedragonladis@gmail.com>
2025-05-25 22:05:32 +0200

    Initial commit.

---
+++ CampaignRegistry.lua
@@ -0,0 +1,50 @@
+{
+	["sidebarvisibility"] = 1,
+	["sidebarexpand"] = {
+		[1] = "tool",
+		[2] = "library",
+		[3] = "campaign",
+		[4] = "world",
+		[5] = "create",
+		[6] = "player",
+	},
+	["colortables"] = {
+		[""] = {
+			["color"] = "FF000000",
+			["diceskin"] = 107,
+			["dicebodycolor"] = "FF000000",
+			["dicetextcolor"] = "FFFFFFFF",
+		},
+	},
+	["windowstate"] = {
+		["tokenbag"] = {
+			["viewzoom"] = 1,
+			["viewtype"] = "grid",
+			["history"] = "sPath|=|Letter Tokens/Medium|&|nDisplayIndex|=|7|&|sFilterType|=|token|||sPath|=|Letter Tokens/Medium|&|sFilterType|=|token|||sPath|=|Letter Tokens/Medium|&|nDisplayIndex|=|7|&|sFilterType|=|token|||sFilterType|=|image|||sPath|=|Data|&|sFilterType|=|image|||sPath|=|Data/npcs|&|sFilterType|=|image|||sFilterType|=|token|||sPath|=|Data|&|sFilterType|=|token|||sFilterType|=|image|||sPath|=|Campaign|&|sFilterType|=|image|||",
+		},
+		["masterindex"] = {
+			["notes"] = {
+				["category"] = "",
+			},
+			["item"] = {
+				["category"] = "Kingmaker",
+			},
+			["npc"] = {
+				["category"] = "Kingmaker",
+			},
+			["quest"] = {
+				["category"] = "Kingmaker",
+			},
+			["treasureparcels"] = {
+				["category"] = "Kingmaker",
+			},
+		},
+	},
+	["tabletopstate"] = {
+		[1] = {
+			["path"] = "image.id-00003",
+			["class"] = "imagewindow",
+		},
+	},
+	["sidebarversion"] = 2,
+}
